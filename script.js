82d38f2cf6948838a50a8218c5ddef8359736664
Liubov Kovalova <liuba532013@gmail.com>
2024-09-18 13:07:54 -0300

    Added Function to populate cuisine select dropdown

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 const apiKey = "a5e21e64c2ae41bbb56740609a2a2e85";
+
+const cuisines = [
+  "African",
+  "Asian",
+  "American",
+  "British",
+  "Cajun",
+  "Caribbean",
+  "Chinese",
+  "Eastern European",
+  "European",
+  "French",
+  "German",
+  "Greek",
+  "Indian",
+  "Irish",
+  "Italian",
+  "Japanese",
+  "Jewish",
+  "Korean",
+  "Latin American",
+  "Mediterranean",
+  "Mexican",
+  "Middle Eastern",
+  "Nordic",
+  "Southern",
+  "Spanish",
+  "Thai",
+  "Vietnamese",
+];
+
+function populateCuisineSelect() {
+  const cuisineSelect = document.getElementById("cuisineSelect");
+  if (!cuisineSelect) return;
+
+  cuisines.forEach((cuisine) => {
+    const option = document.createElement("option");
+    option.value = cuisine.toLowerCase().replace(/\s+/g, "");
+    cuisineSelect.appendChild(option);
+  });
+}
